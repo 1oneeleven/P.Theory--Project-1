5b325cbf39103a95cec81af96e2a922a30ff1e68
1oneeleven <lgnd_247@icloud.com>
2024-12-30 16:07:48 +0000

    Create TheoryProject1.py

---
+++ TheoryProject1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 09:49:28 2024
+
+@author: alialshehhi
+"""
+#this is my first programming theory git project (portfolia task 1)
+
+print("this is my first push for this project")
+
